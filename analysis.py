2cbb79d8ebd0243f35b9f7ed1acf514c67d2ca9d
vanshikakyatam <vanshikakyatam04@gmail.com>
2026-03-22 16:11:52 +0530

    Improved code structure and added full analysis

--- analysis.py
+++ analysis.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#Load dataset
+# Load dataset
 df = pd.read_csv("train.csv")
 
-# Show first rows
+# Show basic info
 print(df.head())
 print(df.columns)
 print(df.info())
@@ -11,20 +12,34 @@ print(df.info())
 # Total Sales
 print("Total Sales:", df['Sales'].sum())
 
+# Top 5 Products
+top_products = df.groupby('Product Name')['Sales'].sum().sort_values(ascending=False).head()
+
+top_products.plot(kind="bar")
+plt.title("Top 5 Products")
+plt.xlabel("Product Name")
+plt.ylabel("Sales")
+plt.show()
+
+
 # Sales by Region
-print(df.groupby('Region')['Sales'].sum())
+df.groupby('Region')['Sales'].sum().plot(kind='bar')
+
+plt.title("Sales by Region")
+plt.xlabel("Region")
+plt.ylabel("Sales")
+plt.show()
 
 
-# Monthly Trend Sales
-df['Order Date']= pd.to_datetime(df['Order Date'],dayfirst=True)
+# Monthly Sales Trend
+df['Order Date'] = pd.to_datetime(df['Order Date'], dayfirst=True)
+
 monthly_sales = df.groupby(df['Order Date'].dt.month)['Sales'].sum()
 
-import matplotlib.pyplot as plt
-
+print(monthly_sales)
 
 monthly_sales.plot(kind='line')
 plt.title("Monthly Sales Trend")
 plt.xlabel("Month")
 plt.ylabel("Sales")
 plt.show()
-
